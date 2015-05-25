07d76dff3d989682da3d533d48090cea8289b10b
Chris George <chris.a.george@gmail.com>
2015-05-24 20:02:32 -0700

    * Finalize test cases and assume twilio is fine

--- tests/index.js
+++ tests/index.js
@@ -1,13 +1,13 @@
 var twilioHelper = require('../lib');
 var demand = require('must');
+var assert = require("assert");
 
 describe('twilio-helper', function () {
   it('should exist', function () {
-    console.log(twilioHelper);
     demand(twilioHelper).to.exist();
   });
 
-  it('success case', function(){
+  it('Get Number from area with available area codes', function(done){
     // checked for success
     var numberToPurchase = '+16196210102';
     twilioHelper.purchasePhoneNumberAsync({
@@ -15,18 +15,24 @@ describe('twilio-helper', function () {
     }).then(function(purchasedNumber) {
       demand(purchasedNumber).to.exist();
       assert.equal(purchasedNumber, numberToPurchase);
+      done();
+    }).catch(function(e){
+      console.log(e);
+      done();
     });
   });
 
-  it('Number Fails to purchase', function(){
+  it('Find number in same state and purchase', function(done){
     // checked for success
     var numberToPurchase = '+12122123456';
     twilioHelper.purchasePhoneNumberAsync({
       nearPhoneNumber: numberToPurchase
-    }).catch(function(err){
-      demand(err).to.exist();
-      // figure out what err is
-      console.log(err);
+    }).then(function(purchasedNumber){
+      assert.notEqual(purchasedNumber, numberToPurchase);
+      done();
+    }).catch(function(e){
+      console.log(e);
+      done();
     });
   });
 });
